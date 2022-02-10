dc60f20661063db25a2efca598cde23f414cb1ec
hwayeon <hwayeon351@gmail.com>
2022-02-10 21:47:46 +0900

    Feat: add Retrofit Interface, Dto and data model

--- app/src/main/java/com/example/book_review_app/MainActivity.kt
+++ app/src/main/java/com/example/book_review_app/MainActivity.kt
@@ -2,10 +2,57 @@ package com.example.book_review_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.book_review_app.api.BookService
+import com.example.book_review_app.model.BestSellerDto
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://book.interpark.com")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val bookService = retrofit.create(BookService::class.java)
+
+        bookService.getBestSellerBooks("6F74C302D57225F07B144534D2062F32F515D5B12E9F364F600B61E9BD92868B")
+            .enqueue(object: Callback<BestSellerDto> {
+                override fun onResponse(
+                    call: Call<BestSellerDto>,
+                    response: Response<BestSellerDto>
+                ) {
+                    //TODO("Not yet implemented")
+                    if (response.isSuccessful.not()) {
+                        Log.e(TAG, "NOT!! SUCCESS")
+                        return
+                    }
+
+                    response.body()?.let {
+                        Log.d(TAG, it.toString())
+                        it.books.forEach { book ->
+                            Log.d(TAG, book.toString())
+                        }
+                    }
+                }
+
+                override fun onFailure(call: Call<BestSellerDto>, t: Throwable) {
+                    //TODO("Not yet implemented")
+                    Log.e(TAG, t.toString())
+                }
+
+            })
     }
+
+    companion object {
+        private const val TAG = "MainActivity"
+    }
+
 }
